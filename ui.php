3bf7478810860502b20ea0ac1b1a9a8673168c61
onyzhin <o.nyzhin@gmail.com>
2015-11-24 10:41:36 +0200

    ui page formed ver 1.0

---
+++ ui.php
@@ -0,0 +1,69 @@
+<div class="main">
+	<div class="container">
+		<div class="content ctext">
+			<h1></h1>
+			<h2></h2>
+			<h3></h3>
+			<h3></h3>
+			<br>
+			<br>
+			<br>
+			<p></p>
+			<p><small></small></p>
+			<p><em></em></p>
+			<br>
+			<br>
+			<br>
+			<a href="" class="button"></a>
+			<a href="" class="button btn-hover"></a>
+			<a href="" class="button btn-gray"></a>
+			<br>
+			<br>
+			<br>
+			<div class="slider">
+				<div><img src="pictures/slider-1.png" height="400" width="924" alt=""></div>
+				<div><img src="pictures/slider-1.png" alt=""></div>
+			</div>
+			<table>
+				<tr>
+					<th>Заголовок</th>
+					<th>Текст</th>
+					<th>Текст</th>
+					<th>Текст</th>
+				</tr>
+				<tr>
+					<td>Заголовок</td>
+					<td>Текст</td>
+					<td>Текст</td>
+					<td>Текст</td>
+				</tr>
+				<tr>
+					<td>Заголовок</td>
+					<td>Текст</td>
+					<td>Текст</td>
+					<td>Текст</td>
+				</tr>
+				<tr>
+					<td>Заголовок</td>
+					<td>Текст</td>
+					<td>Текст</td>
+					<td>Текст</td>
+				</tr>
+			</table>
+			<br>
+			<br>
+			<br>
+			<a href="">Стандарт</a>
+			<blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+				<footer>― <cite>Neil Gaiman, The Sandman</cite></footer>
+			</blockquote>
+			<p></p>
+			<dl>
+				<dt><img src="pictures/cimage.png" height="139" width="160" alt=""></dt>
+				<dd>Russian expansion into the Buryat area around Lake Baikal in 1628–1658 was part of the Russian conquest of Siberia. It was done first by following the Angara River upstream from Yeniseysk (founded 1619) and later by moving south from the Lena River. </dd>
+			</dl>
+			<p></p>
+			<p></p>
+		</div>
+	</div>
+</div>
