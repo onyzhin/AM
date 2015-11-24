df55b18e11d1a61515daa86787d6957215628efe
onyzhin <o.nyzhin@gmail.com>
2015-11-24 10:24:48 +0200

    fuck this shit

---
+++ board.php
@@ -0,0 +1,40 @@
+<div class="main">
+	<div class="container">
+		<div class="content ctext">
+			<div class="mainheading"><em>Board of </em> directors</div>
+			<div class="board-list">
+				<div class="board-item">
+					<div class="board-item-head">
+						<div class="board-item-image"><img src="http://lorempixel.com/172/172/" alt=""></div>
+						<div class="board-item-text">
+							<div class="board-item-title">Mark Zuckerberg </div>
+							<div class="board-item-subtitle">Founder, Chairman and Chief Executive Officer</div>
+						</div>
+					</div>	
+					<div class="board-item-body"><p>Mark is the Founder, Chairman and CEO of Facebook, which he founded in 2004. Mark is responsible for setting the overall direction and product strategy for the company. He leads the design of Facebook's service and development of its core technology and infrastructure. Mark studied computer science at Harvard University before moving the company to Palo Alto, California</p></div>
+				</div>
+				<div class="board-item">
+					<div class="board-item-head">
+						<div class="board-item-image"><img src="http://lorempixel.com/172/172" alt=""></div>
+						<div class="board-item-text">
+							<div class="board-item-title">Mark Zuckerberg </div>
+							<div class="board-item-subtitle">Founder, Chairman and Chief Executive Officer</div>
+						</div>
+					</div>	
+					<div class="board-item-body"><p>Mark is the Founder, Chairman and CEO of Facebook, which he founded in 2004. Mark is responsible for setting the overall direction and product strategy for the company. He leads the design of Facebook's service and development of its core technology and infrastructure. Mark studied computer science at Harvard University before moving the company to Palo Alto, California</p></div>
+				</div>
+			</div>
+		</div>
+		<div class="sidebar">
+			<div class="sidebar-menu">
+				<div class="sidebar-title">About Us</div>
+				<div class="sidebar-list">
+					<ul>
+						<li class="active"><a href="#"><span>Board of directors</span></a></li>
+						<li><a href="#"><span>Investment Manager</span></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ partials/slider.php
@@ -0,0 +1,4 @@
+<div class="slider">
+	<div><img src="pictures/slider-1.png" alt="" /></div>
+	<div><img src="pictures/slider-2.png" alt="" /></div>
+</div>
