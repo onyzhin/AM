03abdd6353a28f32d07fa879345a87f0cf3f6d8a
onyzhin <o.nyzhin@gmail.com>
2015-11-24 18:23:14 +0200

    contacts ver 1.0

---
+++ contacts.php
@@ -0,0 +1,45 @@
+<div class="firstScreen">
+	<div class="slider-back">
+		<div style="background-image: url(../pictures/slider-bg-1.png);"></div>
+		<div style="background-image: url(../pictures/slider-bg-2.jpg);"></div>
+	</div>
+	<div class="container firstScreen-text ctext">
+		<h1><em>Наши</em> Контакты</h1>
+		<p>For questions or inquiries specific to Investor Relations or shareholder matters, please submit your question using the form below.</p>
+		<p>Если Вас заинтересовали наши продукты, и Вы хотите получить более подробную информацию, свяжитесь с нашими консультантами любым удобным для Вас способом.</p>
+	</div>
+</div>
+<div class="main">
+	<div class="container">
+		<div class="contacts">
+			<div class="contacts-data">
+				1
+			</div>
+			<div class="contacts-form">
+				<form action="">
+					<div class="contacts-form-main">
+						<div class="contacts-form-field">
+							<select name="" id="">
+								<option value="">Subject</option>
+								<option value=""></option>
+							</select>
+						</div>
+						<div class="contacts-form-field">
+							<div class="contacts-form-row col2Item">
+								<div class="contacts-form-col"><input type="text" placeholder="Name" /></div>
+								<div class="contacts-form-col"><input type="text" placeholder="E-mail" /></div>
+							</div>
+						</div>
+						<div class="contacts-form-field">
+							<textarea name="" id="" rows="10" placeholder="Comments" /></textarea>
+						</div>
+					</div>
+					<input type="submit" value="Submit comment" />
+				</form>
+			</div>
+		</div>
+		<div class="map">
+			<div id="map"></div>
+		</div>
+	</div>
+</div>
